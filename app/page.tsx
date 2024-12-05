58dabc5a4739afa265e4e49b8714970aee11a479
shezafatima <160762418+shezafatima@users.noreply.github.com>
2024-12-05 20:50:24 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Services from "./components/services";
+import Contact from "./components/contact";
+import Skills from "./components/skills";
+import Projects from "./components/projects";
+import Footer from "./components/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Services/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
